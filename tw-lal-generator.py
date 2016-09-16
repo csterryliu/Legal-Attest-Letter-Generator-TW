08c56f35b0c5bafecf453c8351227cf1ca8a4ef4
Terry_Liu <tkuterry.liu@gmail.com>
2016-09-16 08:29:31 +0800

    [PEP8] Invalid variable name

--- tw-lal-generator.py
+++ tw-lal-generator.py
@@ -9,46 +9,46 @@ from lal_modules.constants import *
 codec_name = 'utf-8'
 
 def process_args():
-    argParser = argparse.ArgumentParser(description=u'台灣郵局存證信函產生器',
-                                        add_help=False)
-    argParser.add_argument('--help',
-                           action='help',
-                           help=u'顯示使用說明')
-    argParser.add_argument('article_file',
-                           action='store',
-                           help=u'存證信函全文之純文字檔路徑')
-    argParser.add_argument('--senderName',
-                           action='append',
-                           nargs='+',
-                           metavar=u'寄件人姓名',
-                           default=[])
-    argParser.add_argument('--senderAddr',
-                           action='append',
-                           metavar=u'寄件人詳細地址',
-                           default=[])
-    argParser.add_argument('--receiverName',
-                           action='append',
-                           nargs='+',
-                           metavar=u'收件人姓名',
-                           default=[])
-    argParser.add_argument('--receiverAddr',
-                           action='append',
-                           metavar=u'收件人詳細地址',
-                           default=[])
-    argParser.add_argument('--ccName',
-                           action='append',
-                           nargs='+',
-                           metavar=u'副本收件人姓名',
-                           default=[])
-    argParser.add_argument('--ccAddr',
-                           action='append',
-                           metavar=u'副本收件人詳細地址',
-                           default=[])
-    argParser.add_argument('--outputFileName',
-                           action='store',
-                           metavar=u'輸出之檔案名稱',
-                           default='output.pdf')
-    return argParser.parse_args()
+    arg_parser = argparse.ArgumentParser(description=u'台灣郵局存證信函產生器',
+                                         add_help=False)
+    arg_parser.add_argument('--help',
+                            action='help',
+                            help=u'顯示使用說明')
+    arg_parser.add_argument('article_file',
+                            action='store',
+                            help=u'存證信函全文之純文字檔路徑')
+    arg_parser.add_argument('--senderName',
+                            action='append',
+                            nargs='+',
+                            metavar=u'寄件人姓名',
+                            default=[])
+    arg_parser.add_argument('--senderAddr',
+                            action='append',
+                            metavar=u'寄件人詳細地址',
+                            default=[])
+    arg_parser.add_argument('--receiverName',
+                            action='append',
+                            nargs='+',
+                            metavar=u'收件人姓名',
+                            default=[])
+    arg_parser.add_argument('--receiverAddr',
+                            action='append',
+                            metavar=u'收件人詳細地址',
+                            default=[])
+    arg_parser.add_argument('--ccName',
+                            action='append',
+                            nargs='+',
+                            metavar=u'副本收件人姓名',
+                            default=[])
+    arg_parser.add_argument('--ccAddr',
+                            action='append',
+                            metavar=u'副本收件人詳細地址',
+                            default=[])
+    arg_parser.add_argument('--outputFileName',
+                            action='store',
+                            metavar=u'輸出之檔案名稱',
+                            default='output.pdf')
+    return arg_parser.parse_args()
 
 def is_only_one_name_or_address(namelist, addresslist):
     ret_value = True
@@ -59,12 +59,12 @@ def is_only_one_name_or_address(namelist, addresslist):
     return ret_value
 
 def read_main_article(filepath):
-    BOM = b'\xef\xbb\xbf'.decode(codec_name)
+    bom = b'\xef\xbb\xbf'.decode(codec_name)
     text_file = open(filepath, 'r', encoding=codec_name)
     text = text_file.read()
     text_file.close()
     # In case the user insists on using Notepad of Windows, remove BOM
-    text = text.lstrip(BOM)
+    text = text.lstrip(bom)
     return text
 
 def parse_main_article(painter, pagePick, mainText):
@@ -88,9 +88,9 @@ def parse_main_article(painter, pagePick, mainText):
     pagePick.pickIndividualPages([0])
 
 def get_new_line_coordinate(currentY):
-    newX = CONTENT_X_Y_BEGIN[0]
-    newY = currentY - (CONTENT_X_Y_INTERVAL[1] + CONTENT_X_Y_FIX[1])
-    return newX, newY
+    new_x = CONTENT_X_Y_BEGIN[0]
+    new_y = currentY - (CONTENT_X_Y_INTERVAL[1] + CONTENT_X_Y_FIX[1])
+    return new_x, new_y
 
 def reset_coordinates_and_counters():
     return CONTENT_X_Y_BEGIN[0], CONTENT_X_Y_BEGIN[1], 1, 1
@@ -139,8 +139,8 @@ def fill_name_address_in_info_box(x_begin, y_begin, namelist, addresslist):
         y_begin -= detail_y_interval
 
     for i in range(max_count):
-        allName = ' '.join(namelist[i]) if i <= len(namelist)-1 else ''
-        generator.drawString(x_begin, y_begin, u'姓名：' + allName)
+        all_name = ' '.join(namelist[i]) if i <= len(namelist)-1 else ''
+        generator.drawString(x_begin, y_begin, u'姓名：' + all_name)
         y_begin -= detail_y_interval
         address = addresslist[i] if i <= len(addresslist)-1 else ''
         generator.drawString(x_begin, y_begin, u'詳細地址：' + address)
@@ -150,10 +150,10 @@ def fill_name_address_in_info_box(x_begin, y_begin, namelist, addresslist):
 
 def fill_name_address_on_first_page(namelist, addresslist, type):
     if len(namelist) == 1:
-        allName = ' '.join(namelist[0])
+        all_name = ' '.join(namelist[0])
         generator.drawString(NAME_COORDINATE[type+'_x_y_begin'][0],
                              NAME_COORDINATE[type+'_x_y_begin'][1],
-                             allName)
+                             all_name)
     if len(addresslist) == 1:
         generator.drawString(ADDR_COORDINATE[type+'_x_y_begin'][0],
                              ADDR_COORDINATE[type+'_x_y_begin'][1],
