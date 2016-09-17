824271adde6f33f76b151ee01c603f1a6df45dc2
Terry_Liu <tkuterry.liu@gmail.com>
2016-09-17 18:03:43 +0800

    [PEP8] No space allowed before bracket [PEP8] Line too long [PEP8] Invalid attribute name [PEP8] Invalid variable and argument name

--- lal_modules/pdfpage.py
+++ lal_modules/pdfpage.py
@@ -7,28 +7,28 @@ class PDFPagePick:
     class PDFPagePick
     Select any page you want and then organize them into a new PDF file
     """
-    def __init__(self, src, outputFileName):
+    def __init__(self, src, output_filename):
         self.__src = PdfFileReader(src)
         self.__output = PdfFileWriter()
-        self.__outputFileName = outputFileName
+        self.__output_filename = output_filename
 
-    def pickIndividualPages(self, pageNumList):
-        for pageNum in pageNumList:
-            if self.__checkPageNum(self.__src, pageNum) == True:
-                self.__output.addPage(self.__src.getPage(pageNum))
+    def pickIndividualPages(self, page_num_list):
+        for page_num in page_num_list:
+            if self.__checkPageNum(self.__src, page_num) == True:
+                self.__output.addPage(self.__src.getPage(page_num))
             else:
-                print ('pageNum %d doesn''t exist. Pass' % pageNum)
+                print('pageNum %d doesn''t exist. Pass' % page_num)
 
     def insertBlankPage(self):
         self.__output.addBlankPage()
 
     def save(self):
-        outputStream = open(self.__outputFileName, 'wb')
-        self.__output.write(outputStream)
-        outputStream.close()
+        outputstream = open(self.__output_filename, 'wb')
+        self.__output.write(outputstream)
+        outputstream.close()
 
-    def __checkPageNum(self, target, pageNum):
-        if pageNum < 0 or pageNum > target.getNumPages()-1:
+    def __checkPageNum(self, target, page_num):
+        if page_num < 0 or page_num > target.getNumPages()-1:
             print('Invalid pageNum')
             return False
         return True
@@ -38,28 +38,29 @@ class PDFPageMerge:
     class PDFMerge
     It merges 2 pdf files into a new one.
     """
-    def __init__(self, src, dest, outputFileName):
+    def __init__(self, src, dest, output_filename):
         self.__src = PdfFileReader(src)
         self.__dest = PdfFileReader(dest)
-        self.__outputFileName = outputFileName
+        self.__output_filename = output_filename
         self.__output = PdfFileWriter()
 
-    def mergeSrcPageToDestPage(self, srcPageNum, destPageNum):
-        if self.__checkPageNum(self.__src, srcPageNum) == True and self.__checkPageNum(self.__dest, destPageNum) == True:
-            destPage = self.__dest.getPage(destPageNum)
-            destPage.mergePage(self.__src.getPage(srcPageNum))
-            self.__output.addPage(destPage)
+    def mergeSrcPageToDestPage(self, src_page_num, dest_page_num):
+        if (self.__checkPageNum(self.__src, src_page_num) == True
+                and self.__checkPageNum(self.__dest, dest_page_num) == True):
+            dest_page = self.__dest.getPage(dest_page_num)
+            dest_page.mergePage(self.__src.getPage(src_page_num))
+            self.__output.addPage(dest_page)
 
     def getSrcTotalPage(self):
         return self.__src.getNumPages()
 
     def save(self):
-        outputStream = open(self.__outputFileName, 'wb')
-        self.__output.write(outputStream)
-        outputStream.close()
+        outputstream = open(self.__output_filename, 'wb')
+        self.__output.write(outputstream)
+        outputstream.close()
 
-    def __checkPageNum(self, target, pageNum):
-        if pageNum < 0 or pageNum > target.getNumPages()-1:
+    def __checkPageNum(self, target, page_num):
+        if page_num < 0 or page_num > target.getNumPages()-1:
             print('Invalid pageNum')
             return False
         return True
