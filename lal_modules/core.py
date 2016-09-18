6abbdb1d734639482426d68270e8b5369b7a0a16
Terry_Liu <tkuterry.liu@gmail.com>
2016-09-18 14:51:33 +0800

    do refactoring on core functions

--- lal_modules/core.py
+++ lal_modules/core.py
@@ -6,35 +6,6 @@ from . import pdfpage
 from . import pdfpainter
 from .constants import *
 
-def generate_text_and_letter(senders, senders_addr,
-                             receivers, receivers_addr,
-                             ccs, cc_addr,
-                             main_text):
-    generator = pdfpainter.PDFPainter(GENERATED_TEXT_PATH,
-                                      LETTER_FORMAT_WIDE_HEIGHT[0], LETTER_FORMAT_WIDE_HEIGHT[1])
-    blank_letter_producer = pdfpage.PDFPagePick(LETTER_FORMAT_PATH, GENERATED_BLANK_LETTER_PATH)
-
-    # write name and address directly if one page is enough
-    one_page_is_enough = is_only_one_name_or_address(senders, senders_addr) and \
-                      is_only_one_name_or_address(receivers, receivers_addr) and \
-                      is_only_one_name_or_address(ccs, cc_addr)
-    if one_page_is_enough:
-        generator.set_font(DEFAULT_FONT_PATH, 10)
-        fill_name_address_on_first_page(generator, senders, senders_addr, 's')
-        fill_name_address_on_first_page(generator, receivers, receivers_addr, 'r')
-        fill_name_address_on_first_page(generator, ccs, cc_addr, 'c')
-
-    generator.set_font(DEFAULT_FONT_PATH, 20)
-    parse_main_article(generator, blank_letter_producer, main_text)
-
-    if one_page_is_enough is False:
-        draw_info_box(generator, senders, senders_addr, receivers, receivers_addr, ccs, cc_addr)
-        generator.end_this_page()
-        blank_letter_producer.insert_blank_page()
-
-    blank_letter_producer.save()
-    generator.save()
-
 def read_main_article(filepath):
     codec_name = 'utf-8'
     bom = b'\xef\xbb\xbf'.decode(codec_name)
@@ -45,7 +16,49 @@ def read_main_article(filepath):
     text = text.lstrip(bom)
     return text
 
-def is_only_one_name_or_address(namelist, addresslist):
+def merge_text_and_letter(output_filename):
+    print('Merging...')
+    page_merge = pdfpage.PDFPageMerge(GENERATED_TEXT_PATH,
+                                      GENERATED_BLANK_LETTER_PATH,
+                                      output_filename)
+    for i in range(page_merge.get_src_total_page()):
+        page_merge.merge_src_page_to_dest_page(i, i)
+    page_merge.save()
+
+def clean_temp_files():
+    remove(GENERATED_TEXT_PATH)
+    remove(GENERATED_BLANK_LETTER_PATH)
+
+def generate_text_and_letter(senders, senders_addr,
+                             receivers, receivers_addr,
+                             ccs, cc_addr,
+                             main_text):
+    generator = pdfpainter.PDFPainter(GENERATED_TEXT_PATH,
+                                      LETTER_FORMAT_WIDE_HEIGHT[0], LETTER_FORMAT_WIDE_HEIGHT[1])
+    blank_letter_producer = pdfpage.PDFPagePick(LETTER_FORMAT_PATH, GENERATED_BLANK_LETTER_PATH)
+
+    # write name and address directly if one page is enough
+    one_page_is_enough = _is_only_one_name_or_address(senders, senders_addr) and \
+                      _is_only_one_name_or_address(receivers, receivers_addr) and \
+                      _is_only_one_name_or_address(ccs, cc_addr)
+    if one_page_is_enough:
+        generator.set_font(DEFAULT_FONT_PATH, 10)
+        _fill_name_address_on_1st_page(generator, senders, senders_addr, 's')
+        _fill_name_address_on_1st_page(generator, receivers, receivers_addr, 'r')
+        _fill_name_address_on_1st_page(generator, ccs, cc_addr, 'c')
+
+    generator.set_font(DEFAULT_FONT_PATH, 20)
+    _parse_main_article(generator, blank_letter_producer, main_text)
+
+    if one_page_is_enough is False:
+        _draw_info_box(generator, senders, senders_addr, receivers, receivers_addr, ccs, cc_addr)
+        generator.end_this_page()
+        blank_letter_producer.insert_blank_page()
+
+    blank_letter_producer.save()
+    generator.save()
+
+def _is_only_one_name_or_address(namelist, addresslist):
     ret_value = True
     if len(namelist) != 0:
         ret_value = ret_value and (len(namelist) == 1)
@@ -53,14 +66,12 @@ def is_only_one_name_or_address(namelist, addresslist):
         ret_value = ret_value and (len(addresslist) == 1)
     return ret_value
 
-
-
-def parse_main_article(painter, page_pick, main_text):
+def _parse_main_article(painter, page_pick, main_text):
     print('Parse main article...')
-    x_begin, y_begin, line_counter, char_counter = reset_coordinates_and_counters()
+    x_begin, y_begin, line_counter, char_counter = _reset_coordinates_and_counters()
     for i in range(0, len(main_text)):
         if main_text[i] == '\n' or (char_counter > CONTENT_MAX_CHARACTER_PER_LINE):
-            x_begin, y_begin = get_new_line_coordinate(y_begin)
+            x_begin, y_begin = _get_new_line_coordinate(y_begin)
             line_counter = line_counter + 1
             char_counter = 1
             if main_text[i] == '\n':
@@ -68,25 +79,25 @@ def parse_main_article(painter, page_pick, main_text):
         if line_counter > CONTENT_MAX_LINE_PER_PAGE:
             painter.end_this_page()
             page_pick.pick_individual_pages([0])
-            x_begin, y_begin, line_counter, char_counter = reset_coordinates_and_counters()
+            x_begin, y_begin, line_counter, char_counter = _reset_coordinates_and_counters()
         painter.draw_string(x_begin, y_begin, main_text[i])
         x_begin += (CONTENT_X_Y_INTERVAL[0] - CONTENT_X_Y_FIX[0])
         char_counter = char_counter + 1
     painter.end_this_page()
     page_pick.pick_individual_pages([0])
 
-def get_new_line_coordinate(current_y):
+def _get_new_line_coordinate(current_y):
     new_x = CONTENT_X_Y_BEGIN[0]
     new_y = current_y - (CONTENT_X_Y_INTERVAL[1] + CONTENT_X_Y_FIX[1])
     return new_x, new_y
 
-def reset_coordinates_and_counters():
+def _reset_coordinates_and_counters():
     return CONTENT_X_Y_BEGIN[0], CONTENT_X_Y_BEGIN[1], 1, 1
 
-def draw_info_box(painter,
-                  sender_list, sender_addr_list,
-                  receiver_list, receiver_addr_list,
-                  cc_list, cc_addr_list):
+def _draw_info_box(painter,
+                   sender_list, sender_addr_list,
+                   receiver_list, receiver_addr_list,
+                   cc_list, cc_addr_list):
     painter.set_font(DEFAULT_FONT_PATH, 8)
     painter.draw_string(CUT_INFO_X_Y[0], CUT_INFO_X_Y[1], u'[請自行剪下貼上]')
     painter.draw_line(BOX_UPPDERLEFT_X_Y[0], BOX_UPPDERLEFT_X_Y[1],
@@ -100,15 +111,15 @@ def draw_info_box(painter,
     painter.draw_string(TITLE_START[0], TITLE_START[1], u'一、寄件人')
     x_begin = DETAIL_START[0]
     y_begin = DETAIL_START[1]
-    y_begin = fill_name_address_in_info_box(painter,
-                                            x_begin, y_begin,
-                                            sender_list, sender_addr_list)
+    y_begin = _fill_name_address_in_info_box(painter,
+                                             x_begin, y_begin,
+                                             sender_list, sender_addr_list)
 
     y_begin -= TITLE_Y_INTERVAL
     painter.draw_string(TITLE_START[0], y_begin, u'二、收件人')
-    y_begin = fill_name_address_in_info_box(painter,
-                                            x_begin, y_begin,
-                                            receiver_list, receiver_addr_list)
+    y_begin = _fill_name_address_in_info_box(painter,
+                                             x_begin, y_begin,
+                                             receiver_list, receiver_addr_list)
 
     y_begin -= TITLE_Y_INTERVAL
     painter.draw_string(TITLE_START[0], y_begin, u'三、')
@@ -116,9 +127,9 @@ def draw_info_box(painter,
                         y_begin+CC_RECEIVER_FIX_X_Y[1], u'副 本')
     painter.draw_string(TITLE_START[0]+CC_RECEIVER_FIX_X_Y[0],
                         y_begin-CC_RECEIVER_FIX_X_Y[1], u'收件人')
-    y_begin = fill_name_address_in_info_box(painter,
-                                            x_begin, y_begin,
-                                            cc_list, cc_addr_list)
+    y_begin = _fill_name_address_in_info_box(painter,
+                                             x_begin, y_begin,
+                                             cc_list, cc_addr_list)
 
     painter.draw_line(BOX_UPPDERLEFT_X_Y[0], BOX_UPPDERLEFT_X_Y[1],
                       BOX_UPPDERLEFT_X_Y[0], y_begin)  # left
@@ -127,7 +138,7 @@ def draw_info_box(painter,
     painter.draw_line(BOX_UPPDERRIGHT_X_Y[0], BOX_UPPDERRIGHT_X_Y[1],
                       BOX_UPPDERRIGHT_X_Y[0], y_begin)  # right
 
-def fill_name_address_in_info_box(painter, x_begin, y_begin, namelist, addresslist):
+def _fill_name_address_in_info_box(painter, x_begin, y_begin, namelist, addresslist):
     max_count = max(len(namelist), len(addresslist))
     if max_count == 0:
         painter.draw_string(x_begin, y_begin, u'姓名：')
@@ -145,7 +156,7 @@ def fill_name_address_in_info_box(painter, x_begin, y_begin, namelist, addressli
 
     return y_begin
 
-def fill_name_address_on_first_page(painter, namelist, addresslist, type_):
+def _fill_name_address_on_1st_page(painter, namelist, addresslist, type_):
     if len(namelist) == 1:
         all_name = ' '.join(namelist[0])
         painter.draw_string(NAME_COORDINATE[type_+'_x_y_begin'][0],
@@ -155,17 +166,3 @@ def fill_name_address_on_first_page(painter, namelist, addresslist, type_):
         painter.draw_string(ADDR_COORDINATE[type_+'_x_y_begin'][0],
                             ADDR_COORDINATE[type_+'_x_y_begin'][1],
                             addresslist[0])
-
-
-def merge_text_and_letter(output_filename):
-    print('Merging...')
-    page_merge = pdfpage.PDFPageMerge(GENERATED_TEXT_PATH,
-                                      GENERATED_BLANK_LETTER_PATH,
-                                      output_filename)
-    for i in range(page_merge.get_src_total_page()):
-        page_merge.merge_src_page_to_dest_page(i, i)
-    page_merge.save()
-
-def clean_temp_files():
-    remove(GENERATED_TEXT_PATH)
-    remove(GENERATED_BLANK_LETTER_PATH)
