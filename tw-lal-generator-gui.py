ccc65910c8066905f0f4fdb2d29f5af1d7a29735
Terry_Liu <tkuterry.liu@gmail.com>
2016-09-25 12:41:49 +0800

    Push first GUI code

---
+++ tw-lal-generator-gui.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+
+import tkinter
+from tkinter import ttk
+
+version = 'v1.1.1'
+
+window = tkinter.Tk()
+window.title('台灣郵局存證信函產生器 ' + version)
+window.geometry('600x500')
+
+#name_addr_info = tkinter.LabelFrame(window, text='姓名與地址資訊')
+#name_addr_info.pack(fill = 'y', side = 'top')
+#info_text = tkinter.Text(name_addr_info)
+#info_text.config(state='disable')
+#info_text.config(height = 10)
+#nfo_text.pack()
+
+window.mainloop()
