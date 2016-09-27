a5f779c34c3d17cb413b0710f1053bba93324c4a
Terry_Liu <tkuterry.liu@gmail.com>
2016-09-27 17:46:04 +0800

    Draw initial look

--- tw-lal-generator-gui.py
+++ tw-lal-generator-gui.py
@@ -1,19 +1,36 @@
 #!/usr/local/bin/python3
 
 import tkinter
-from tkinter import ttk
 
 version = 'v1.1.1'
 
 window = tkinter.Tk()
 window.title('台灣郵局存證信函產生器 ' + version)
-window.geometry('600x500')
+window.geometry('600x600')
 
-#name_addr_info = tkinter.LabelFrame(window, text='姓名與地址資訊')
-#name_addr_info.pack(fill = 'y', side = 'top')
-#info_text = tkinter.Text(name_addr_info)
+mainframe = tkinter.Frame(window)
+
+
+menubar = tkinter.Menu(window)
+m_file = tkinter.Menu(menubar)
+m_file.add_command(label='關閉', command=window.quit)
+menubar.add_cascade(label='檔案', menu=m_file)
+window.config(menu=menubar)
+
+
+
+info_frame = tkinter.LabelFrame(mainframe, text='姓名與地址資訊')
+info_text = tkinter.Text(info_frame, height=10)
+info_text.pack()
+info_frame.pack()
 #info_text.config(state='disable')
-#info_text.config(height = 10)
-#nfo_text.pack()
 
+
+article_frame = tkinter.LabelFrame(mainframe, text='內文')
+article_text = tkinter.Text(article_frame)
+article_text.pack(fill='both', expand='yes')
+article_frame.pack(fill='both', expand='yes')
+
+
+mainframe.pack(fill='both', expand='yes')
 window.mainloop()
