6ef36e4115264b799c341d920afa28a47e857013
lyenliang <lyenliang@gmail.com>
2016-10-02 00:10:40 +0800

    1. Add bootstrap 2. Put *.pdf in public folder

--- Web/Gemfile
+++ Web/Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'bootstrap-sass'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
@@ -42,4 +43,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- Web/app/controllers/welcome_controller.rb
+++ Web/app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
+require 'securerandom'
+
 class WelcomeController < ApplicationController
 
 	def index
-		@result = %x(cd $LALGPATH ; python3 $LALGPATH/tw-lal-generator.py $LALGPATH/test.txt 2>&1)
+		@name = SecureRandom.hex(13)
+		@result = %x(cd $LALGPATH ; python3 tw-lal-generator.py test.txt --outputFileName Web/public/#{@name}.pdf 2>&1)
 		@tt = "tttttt"
 	end
 
