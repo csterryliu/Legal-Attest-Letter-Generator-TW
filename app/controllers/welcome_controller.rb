68ecf34d9c569e127a892783a1b29ae2e7d3b42c
lyenliang <lyenliang@gmail.com>
2016-10-02 00:57:04 +0800

    Test heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
 	def index
 		@name = SecureRandom.hex(13)
 		@result = %x(cd $LALGPATH ; python3 tw-lal-generator.py test.txt --outputFileName ../public/#{@name}.pdf 2>&1)
-		@tt = "tttttt"
+		@tt = "tt"
 	end
 
 end
