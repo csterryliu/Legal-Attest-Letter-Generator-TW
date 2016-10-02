01c008e9ed61218569878d15f30c0e9fee1b621e
lyenliang <lyenliang@gmail.com>
2016-10-02 15:32:21 +0800

    Add theme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,6 @@ require 'securerandom'
 
 class WelcomeController < ApplicationController
 
-	def index
-
-	end
-
 	def generate_pdf
 		senderName = params[:senderName]
 		senderAddr = params[:senderAddr]
@@ -14,8 +10,33 @@ class WelcomeController < ApplicationController
 		ccName = params[:ccName]
 		ccAddr = params[:ccAddr]
 
+		command = "python3 tw-lal-generator.py test.txt"
+		
+		if senderName != ''
+			command << " --senderName #{senderName}"
+		end
+		if senderAddr != ''
+			command << " --senderAddr #{senderAddr}"
+		end
+		if receiverName != ''
+			command << " --receiverName #{receiverName}"
+		end
+		if receiverAddr != ''
+			command << " --receiverAddr #{receiverAddr}"
+		end
+		if ccName != ''
+			command << " --ccName #{ccName}"
+		end
+		if ccAddr != ''
+			command << " --ccAddr #{ccAddr}"
+		end
+
 		@name = SecureRandom.hex(13)
-		@result = %x(cd python ; python3 tw-lal-generator.py test.txt --senderName #{senderName} --senderAddr #{senderAddr} --receiverName #{receiverName} --receiverAddr #{receiverAddr} --ccName #{ccName} --ccAddr #{ccAddr} --outputFileName ../public/download/#{@name}.pdf 2>&1)
+
+		@result = %x(cd python ; #{command} --outputFileName ../public/download/#{@name}.pdf 2>&1)
+
+
+		#@result = %x(cd python ; python3 tw-lal-generator.py test.txt --senderName #{senderName} --senderAddr #{senderAddr} --receiverName #{receiverName} --receiverAddr #{receiverAddr} --ccName #{ccName} --ccAddr #{ccAddr} --outputFileName ../public/download/#{@name}.pdf 2>&1)
 		send_file( Rails.root.join('public/download', "#{@name}.pdf"), type: 'application/pdf')
 	end
 
