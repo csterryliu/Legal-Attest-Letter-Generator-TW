f765350327382732a9f37157046960d0c59f7fd0
lyenliang <lyenliang@gmail.com>
2016-10-02 12:24:33 +0800

    Implement basic functions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,15 @@ class WelcomeController < ApplicationController
 	end
 
 	def generate_pdf
+		senderName = params[:senderName]
+		senderAddr = params[:senderAddr]
+		receiverName = params[:receiverName]
+		receiverAddr = params[:receiverAddr]
+		ccName = params[:ccName]
+		ccAddr = params[:ccAddr]
+
 		@name = SecureRandom.hex(13)
-		@result = %x(cd python ; python3 tw-lal-generator.py test.txt --outputFileName ../public/download/#{@name}.pdf 2>&1)
+		@result = %x(cd python ; python3 tw-lal-generator.py test.txt --senderName #{senderName} --senderAddr #{senderAddr} --receiverName #{receiverName} --receiverAddr #{receiverAddr} --ccName #{ccName} --ccAddr #{ccAddr} --outputFileName ../public/download/#{@name}.pdf 2>&1)
 		send_file( Rails.root.join('public/download', "#{@name}.pdf"), type: 'application/pdf')
 	end
 
